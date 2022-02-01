fcdb0f22cfa270f95dcc9e413106a94549658b39
Elygledson <ejsb2@ic.ufal.br>
2022-02-01 19:15:59 -0300

    Add ball movement and game score

--- soccer/main.cpp
+++ soccer/main.cpp
@@ -2,8 +2,28 @@
 #include <gl/glut.h>
 #include <stdlib.h>
 #include<math.h>
+#include <string>
+using namespace std;
 
 
+int homeTeam = 0,awayTeam = 0;
+char txt[30];
+float xmove[]{ 0, 0 };
+float ymove[]{ 0, 0 };
+float zmove[]{ 0, 0 };
+
+
+
+void gameScore(char *String)
+{
+    sprintf(txt,"%d X %d",homeTeam,awayTeam);
+
+  	glPushMatrix();
+        glRasterPos3f(0,7.7,0.5);
+        while(*String)
+             glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,*String++);
+	glPopMatrix();
+}
 
 
 void goalpost()
@@ -26,6 +46,7 @@ void goalpost()
      glVertex3f(-1.5,2.0,13);
     glEnd();
 }
+
 void circle()
 {
     ////Adding a big circle at middle
@@ -46,10 +67,29 @@ void ball()
     //Adding ball
     glPushMatrix();
       glColor3ub(255,255,255);
-      glTranslated(0,0.2,0);
+      glTranslatef(xmove[0], ymove[0], zmove[0]);
       glutSolidSphere(0.40, 100, 100);
     glPopMatrix();
+
+     //Updating game score
+     //Home Team
+     if (zmove[0] > 15)
+     {
+            xmove[0] = 0;
+            ymove[0] = 0;
+            zmove[0] = 0;
+            homeTeam+=1;
+     }
+     //Away Team
+     else if (zmove[0] < -15)
+     {
+          xmove[0] = 0;
+          ymove[0] = 0;
+          zmove[0] = 0;
+          awayTeam+=1;
+     }
 }
+
 
 void pitch()
 {
@@ -73,6 +113,7 @@ void pitch()
      glVertex3f(7,0,0);
    glEnd();
 
+   //Lines within the area
    glBegin(GL_LINE_LOOP);
      glColor3ub(255,255,255);
      glVertex3f(-2.3,0,12.5);
@@ -104,7 +145,10 @@ void pitch()
      glVertex3f(3.5,0,-15);
      glVertex3f(-3.5,0,-15);
    glEnd();
+   //end
 
+   //Add penalty mark
+   glPointSize(3);
    glBegin(GL_POINTS);
     glVertex3f(0,0,11.5);
     glVertex3f(0,0,-11.5);
@@ -120,6 +164,7 @@ void display()
      ball();
      circle();
      goalpost();
+     gameScore(txt);
   glPopMatrix();
 
   glutSwapBuffers();
@@ -137,6 +182,18 @@ void reshape(int w, int h)
 }
 
 
+void keyboard(int button, int x, int y)
+{
+    switch (button)
+    {
+        case GLUT_KEY_UP: xmove[0] -= 0.2; break;
+        case GLUT_KEY_LEFT: zmove[0] += 0.2; break;
+        case GLUT_KEY_RIGHT: zmove[0] -= 0.2; break;
+        case GLUT_KEY_DOWN: xmove[0] += 0.2; break;
+    }
+    glutPostRedisplay();
+}
+
 
 int main(int argc, char **argv)
 {
@@ -148,6 +205,7 @@ int main(int argc, char **argv)
 
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
+  glutSpecialFunc(keyboard);
   glutMainLoop();
   return(0);
 }
